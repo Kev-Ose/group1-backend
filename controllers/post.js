feec25558d6d1806e1b7cc32ffeae8a2b43bf641
ashrafpoless <ashrafpoless@gmail.com>
2024-09-18 16:45:42 +0200

    add and update post

--- controllers/post.js
+++ controllers/post.js
@@ -2,10 +2,48 @@ import query from '../config/db.js';
 
 
 const postsControllers = {
-    getAllPosts: async (req, res) => {},
-    getOnePost: async (req, res) => {},
-    addPost: async (req, res) => {},
-    updatePost: async (req, res) => {},
+    getAllPosts: async (req, res) => {
+        
+    },
+    getOnePost: async (req, res) => {
+        
+    },
+    addPost: async (req, res) => {
+        try {
+            const { username, title, content, img, cat , date, uid} = req.body;
+            if (!username || !title || !content || !img ||!date ||!cat || !uid) {
+                return res
+                    .status(400)
+                    .json({ ok: false, message: `please fill all the field` });
+            } else {
+                const addPost = await query(
+                    'INSERT INTO posts  (title, content, img, date, cat, uid) VALUES (?, ?, ?, ?, ?, ?) ',
+                    [title, content, img, date, cat, uid]
+                );
+                return res.status(200).json({ ok: true, post: req.body });
+            }
+        } catch (error) {
+            return res.status(500).json({ ok: false, message: error.message });
+        }
+    },
+    updatePost: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const { title, content, img, cat} = req.body;
+            if (!title || !content ||!img || !cat ) {
+                return res
+                    .status(404)
+                    .json({  message: `please fill all the field` });
+            } else {
+                const updatePost = await query(
+                    'UPDATE posts SET title = ?, content = ?, img = ?, cat = ?  WHERE id = ?',
+                    [title, content, img, cat, id] );
+                return res.status(200).json({  post: req.body });
+            }
+        } catch (error) {
+            return res.status(500).json({  message: error.message });
+        }
+    },
     deletePost: async (req, res) => {}
 };
 
--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import cors from 'cors';
+
+import { v2 as cloudinary } from 'cloudinary';
+import multer from "multer";
+import {CloudinaryStorage} from 'multer-storage-cloudinary';
 
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+// initialize express
+const app = express();
+
 // import routes
 import postRoutes from './routes/post.js';
 import userRoutes from './routes/user.js'
+
 // set port
+dotenv.config()
 const PORT = process.env.PORT || 5000;
 
 // Construct path
 const __filename = fileURLToPath(import.meta.url);
 const PATH = dirname(__filename);
 
-// initialize express
-const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow this specific origin
+    methods: 'GET,POST,PUT,DELETE',  // Specify allowed HTTP methods
+    credentials: true                // Enable sending cookies with requests
+}));
+
 
 // parse body and cookies
 app.use(express.json());
@@ -26,11 +41,42 @@ app.use(cookieParser());
 app.use(express.static(path.join(PATH, 'public')));
 
 // create tables
-// createUsersTable();
-// createPostsTable();
+createUsersTable();
+createPostsTable();
+
+// configure cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Set up Cloudinary storage for multer
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'blog_images', // The folder where images will be stored on Cloudinary
+        allowed_formats: ['jpg', 'png', 'jpeg'],
+    },
+});
+
+// Set up multer to use Cloudinary storage
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    try {
+        const file = req.file;
+        res.status(200).json({ url: file.path }); // Return Cloudinary URL
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
 
 // use routes
 app.use(userRoutes);
+app.use(postRoutes);
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -41,7 +87,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(postRoutes);
+
 
 // error
 app.use((err, req, res, next) => {
